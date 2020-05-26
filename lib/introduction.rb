cb1481118ee35002ece80dc9c198c085c8c5781f
Michael Harris <mwharris7@gmail.com>
2020-05-26 17:51:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_name(name, language_name)
+def introduction_with_language(name, language_name)
   puts "Hi, my name is #{name} and I am learning to program in #{language_name}."
 end
